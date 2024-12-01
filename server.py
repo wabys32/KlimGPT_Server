62f11a4f9928fecb1b7a551831a69bef6d92740a
wabys32 <artur.tokarev134@gmail.com>
2024-12-01 20:39:42 +0500

    Gemini1

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
 from flask import Flask, request, jsonify
+import google.generativeai as genai
 
 app = Flask(__name__)
+
+genai.configure(api_key="AIzaSyBAC3Q5Bukp7rtZ6xmExAe1VDtWFvrgczA")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 @app.route('/process', methods=['POST'])
 def process_message():
     data = request.get_json()
     message = data.get('message', '')
-
+    
+    response = model.generate_content(message)
     # Logic to process the message
-    response_message = f"Клим говорит: ответ на сообщение {message}"
+    response_message = f"Клим говорит: {response}"
 
     return jsonify({'response': response_message})
 
